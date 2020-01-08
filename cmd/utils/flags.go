fc558bdabefb9bd44f301c111f37de83700530f7
AnilChinchawale <anil24593@gmail.com>
2020-01-08 16:08:25 +0530

    MainNet and Apothem flag added.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -613,14 +613,17 @@ func setBootstrapNodes(ctx *cli.Context, cfg *p2p.Config) {
 		} else {
 			urls = strings.Split(ctx.GlobalString(BootnodesFlag.Name), ",")
 		}
-	case ctx.GlobalBool(TestnetFlag.Name):
-		urls = params.TestnetBootnodes
-	case ctx.GlobalBool(RinkebyFlag.Name):
-		urls = params.RinkebyBootnodes
+	// case ctx.GlobalBool(TestnetFlag.Name):
+	// 	urls = params.TestnetBootnodes
+	// case ctx.GlobalBool(RinkebyFlag.Name):
+	// 	urls = params.RinkebyBootnodes
 	case cfg.BootstrapNodes != nil:
 		return // already set, don't apply defaults.
+	case !ctx.GlobalIsSet(BootnodesFlag.Name):
+		urls = params.MainnetBootnodes
+	case ctx.GlobalBool(XDCTestnetFlag.Name):
+		urls = params.TestnetBootnodes
 	}
-
 	cfg.BootstrapNodes = make([]*discover.Node, 0, len(urls))
 	for _, url := range urls {
 		node, err := discover.ParseNode(url)
@@ -825,7 +828,7 @@ func SetP2PConfig(ctx *cli.Context, cfg *p2p.Config) {
 	setNAT(ctx, cfg)
 	setListenAddress(ctx, cfg)
 	setBootstrapNodes(ctx, cfg)
-	setBootstrapNodesV5(ctx, cfg)
+	// setBootstrapNodesV5(ctx, cfg)
 
 	lightClient := ctx.GlobalBool(LightModeFlag.Name) || ctx.GlobalString(SyncModeFlag.Name) == "light"
 	lightServer := ctx.GlobalInt(LightServFlag.Name) != 0
